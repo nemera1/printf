a4b1e1dfcc28385fc351a03de0d42af68b78cc78
nemera1 <nemeraararsa@gmail.com>
2021-09-06 01:01:47 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,12 @@
  *    *
  *     * Description: uses pointers
  *      * Return: Alawy (0)
- *       */
+ *      */
 
 int _strlen(char *s)
 {
 int i;
-
+/*for loop */
 for (i = 0; s[i] != '\0'; ++i)
 {
 continue;
