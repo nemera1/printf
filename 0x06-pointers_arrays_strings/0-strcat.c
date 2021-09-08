de538b8b8fcc0bc6189d664681751782c78ce649
nemera1 <nemeraararsa@gmail.com>
2021-09-07 23:31:21 -0700

    o strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _strchr - finds if char occurs in string
- *
- * @s: string to check
- * @c: character to find in s
- *
- * Return: pointer to first instance of c, otherwise NULL
+ * _strcat - concatenates two strings
+ * @dest: dest string
+ * @src: src string
+ * Return: pointer to resulting string
  */
-char *_strchr(char *s, char c)
+char *_strcat(char *dest, char *src)
 {
-int i = 0;
+int s = 0, i = 0;
 
-while (*(s + i))
+while (dest[s] != 0)
 {
-if (*(s + i) == c)
-return (s + i);
+s++;
+}
+while (src[i] != 0)
+{
+dest[s + i] = src[i];
 i++;
 }
-if (*(s + i) == c)
-return (s + i);
-return (NULL);
+
+dest[s + i] = 0;
+return (dest);
 }
